29adf18330e8c21f8fdcb6f417aa35b34515a0d5
Hasan Demir <98713315+HasanHuseyinDemir@users.noreply.github.com>
2022-05-27 09:55:34 +0300

    Update app.js

--- Examples/With Module/app.js
+++ Examples/With Module/app.js
@@ -1,6 +1,6 @@
 /*data import example , naming is not important*/
 import _data1_ from "./data/data.js";
 import _data2_ from "./data/data2.js";
-/*  We need to import this as import => {page}  */
+/*  We need to import this */
 import namingItsNotImportant from "./atoms/sample.js"
 
