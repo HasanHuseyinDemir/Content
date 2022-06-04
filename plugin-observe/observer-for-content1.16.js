7c3fdf0cbcdc0cd52a6ec29209a956c1e6db8158
Hasan Demir <98713315+HasanHuseyinDemir@users.noreply.github.com>
2022-06-04 17:07:26 +0300

    Create observer-for-content1.16.js

---
+++ plugin-observe/observer-for-content1.16.js
@@ -0,0 +1,44 @@
+let dataSet = {
+    value:[],
+    get:()=>{
+        dataSet.value.forEach((value)=>{
+            eval(`dataSet.${value}=window.${value}`)
+        })
+    },
+    set:()=>{
+        dataSet.value.forEach((value)=>{
+            if(eval(`dataSet.${value}`)!==eval(`window.${value}`)){      
+                dataSet.get();
+                set();
+                return
+            }
+        })
+    }
+}
+
+class watch{
+    constructor(value,func){
+        this.watching=value;
+        this.value=value.map((item)=>{return eval(item)})
+        this.func=func;
+    }
+    get(){
+        this.value = this.watching.map((item)=>{return eval(item)})
+    }
+    set (){
+        var temp = this.watching.map((item)=>{return eval(item)})
+        if(temp.join("")!==this.value.join("")){      
+            this.get();
+            this.func();
+        }
+    }
+}
+
+w=()=>{
+    watchlist.forEach((item)=>{
+        item.set();
+    });
+    dataSet.set();
+}
+
+let watchlist = [];
